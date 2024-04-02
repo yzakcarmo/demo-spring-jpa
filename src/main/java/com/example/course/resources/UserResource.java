5fad463b89b4446e423667a38dfb013ebc21bb23
Yzak Carmo <yzak.carmo@gmail.com>
2024-04-01 22:37:11 -0300

    Criado o User e o UserResource

---
+++ src/main/java/com/example/course/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.example.course.resources;
+
+import com.example.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "teste", "teste@teste", "999999", "Teste");
+        return ResponseEntity.ok().body(u);
+    }
+}
