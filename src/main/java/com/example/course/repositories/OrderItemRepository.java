8e074229d1839d9ea582654d71c8668c5fd74270
Yzak Carmo <yzak.carmo@gmail.com>
2024-04-07 23:13:30 -0300

    Finalizado a associação de OrderItem e Order

---
+++ src/main/java/com/example/course/repositories/OrderItemRepository.java
@@ -0,0 +1,7 @@
+package com.example.course.repositories;
+
+import com.example.course.entities.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem,Long> {
+}
