5b1502ba89ecba41c3c57e861b1cd93b24fbdde2
Yzak Carmo <yzak.carmo@gmail.com>
2024-04-08 18:49:25 -0300

    Criado o Create, Delete e Update do User

--- src/main/java/com/example/course/services/UserService.java
+++ src/main/java/com/example/course/services/UserService.java
@@ -19,8 +19,27 @@ public class UserService {
     }
 
     public User findById(Long id) {
-
         Optional<User> obj = repository.findById(id);
         return obj.get();
+    }
+
+    public User insert(User obj) {
+        return repository.save(obj);
+    }
+
+    public void remove(Long id) {
+        repository.deleteById(id);
+    }
+
+    public User update(Long id, User obj) {
+        User entity = repository.getReferenceById(id);
+        updateData(entity, obj);
+        return repository.save(entity);
+    }
+
+    public void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
     }
 }
