19baf4d5cd5a8e25a93bb51e24211fea26fa34a4
Yzak Carmo <yzak.carmo@gmail.com>
2024-04-08 21:39:11 -0300

    Tratamento de exceção para o delete

---
+++ src/main/java/com/example/course/services/exceptions/DatabaseException.java
@@ -0,0 +1,8 @@
+package com.example.course.services.exceptions;
+
+public class DatabaseException extends RuntimeException{
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+}
