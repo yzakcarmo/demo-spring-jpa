31da1ea4faff7136830b40a46c9932acb8ec7419
Yzak Carmo <yzak.carmo@gmail.com>
2024-04-08 21:14:41 -0300

    Inicio da implementação do controle de exceção

--- src/main/java/com/example/course/services/UserService.java
+++ src/main/java/com/example/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.example.course.services;
 
 import com.example.course.entities.User;
 import com.example.course.repositories.UserRepository;
+import com.example.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
---
+++ src/main/java/com/example/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(Object id){
+        super("Resource not found. Id " + id);
+    }
+}
